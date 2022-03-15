4bdf76276402e86da99183110cab3f3f5161ce1d
moses <academicsnotes@gmail.com>
2022-03-15 23:19:43 +0800

    Commit scripts

---
+++ src/main/java/playwright_example.java
@@ -0,0 +1,13 @@
+import com.microsoft.playwright.*;
+
+public class playwright_example {
+
+    public static void main(String[] args) {
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium().launch();
+            Page page = browser.newPage();
+            page.navigate("http://playwright.dev");
+            System.out.println(page.title());
+        }
+    }
+}

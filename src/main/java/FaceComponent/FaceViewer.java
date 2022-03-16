1348665bce859f4848b3f6c392f75846d683c10d
moses <academicsnotes@gmail.com>
2022-03-16 23:28:38 +0800

    Commit code

---
+++ src/main/java/FaceComponent/FaceViewer.java
@@ -0,0 +1,17 @@
+package FaceComponent;
+
+import javax.swing.*;
+
+public class FaceViewer {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setSize(300, 400);
+        frame.setTitle("An Alien Face");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        FaceComponent component = new FaceComponent();
+        frame.add(component);
+        frame.setVisible(true);
+    }
+}

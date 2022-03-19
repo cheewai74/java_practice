3c7a749552de0f4b55d901b334aa28bb63506571
moses <academicsnotes@gmail.com>
2022-03-19 13:43:26 +0800

    Commit code for arraylist

---
+++ src/main/java/Arrays/Example3/TicTacToeRunner.java
@@ -0,0 +1,36 @@
+package Arrays.Example3;
+
+import java.util.Scanner;
+
+public class TicTacToeRunner {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        String player = "x";
+        TicTacToe game = new TicTacToe();
+        Boolean done = false;
+
+        while (!done){
+
+            System.out.print(game.toString());
+            System.out.print(
+                    "Row for " + player + " (-1 to exit): "
+            );
+            int row = in.nextInt();
+            if (row < 0) done = true;
+            else
+            {
+                System.out.print("Column for " + player + ": ");
+                int column = in.nextInt();
+                game.set(row, column, player);
+                if(player.equals("x"))
+                    player ="o";
+                else
+                    player="x";
+            }
+        }
+
+
+    }
+}
